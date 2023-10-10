dc8684c352b2eca661445763ab4ad9aa3e058e6e
Manya R Shetty <102415234+manyashetty@users.noreply.github.com>
2023-10-10 22:46:17 +0530

    Create nosemicolon.java
    Printing "Hello World!" without semicolon in java...

---
+++ nosemicolon.java
@@ -0,0 +1,7 @@
+// print "Hello World!" without semicolon in java...
+public class HelloWorld{
+  public static void main(String[] args){
+    if(System.out.printf("Hello World!)==null){
+    }
+  }
+}
